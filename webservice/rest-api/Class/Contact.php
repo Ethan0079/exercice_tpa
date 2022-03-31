6372c82696c8e935141efe9d98e11601bea8046b
Ethan <marchand.ethan@bluewin.ch>
2022-03-31 07:14:04 +0200

    Update Contact.php

--- webservice/rest-api/Class/Contact.php
+++ webservice/rest-api/Class/Contact.php
@@ -1,23 +1,93 @@
+
 <?php
-function create(){
-		
-	$stmt = $this->conn->prepare("
-		INSERT INTO ".$this->itemsTable."(`firstname`, `lastname`, `age`, `email`, `tel_number`)
-		VALUES(?,?,?,?,?)");
+class Contact {   
+  
+    public $id;
+    public $firstname;
+    public $lastname;
+    public $age;
+    public $email;   
+    public $tel_number; 
+
+	private $contactTable = "contact";   
+    private $conn;
 	
-	$this->firstname = htmlspecialchars(strip_tags($this->firstname));
-	$this->lastname = htmlspecialchars(strip_tags($this->lastname));
-	$this->age = htmlspecialchars(strip_tags($this->age));
-	$this->email = htmlspecialchars(strip_tags($this->email));
-	$this->tel_number = htmlspecialchars(strip_tags($this->tel_number));
+    public function __construct($db){
+        $this->conn = $db;
+    }	
 	
-	
-	$stmt->bind_param("ssiis", $this->firstname, $this->lastname, $this->age, $this->email, $this->tel_number);
-	
-	if($stmt->execute()){
-		return true;
+	function read(){	
+		if($this->id) {
+			$stmt = $this->conn->prepare("SELECT * FROM ".$this->itemsTable." WHERE id = ?");
+			$stmt->bind_param("i", $this->id);					
+		} else {
+			$stmt = $this->conn->prepare("SELECT * FROM ".$this->itemsTable);		
+		}		
+		$stmt->execute();			
+		$result = $stmt->get_result();		
+		return $result;	
 	}
- 
-	return false;		 
+	
+	function create(){
+		
+		$stmt = $this->conn->prepare("
+			INSERT INTO ".$this->contactTable."(`firstname`, `lastname`, `age`, `email`, `tel_number`)
+			VALUES(?,?,?,?,?)");
+		
+		$this->firstname = htmlspecialchars(strip_tags($this->firstname));
+		$this->lastname = htmlspecialchars(strip_tags($this->lastname));
+		$this->age = htmlspecialchars(strip_tags($this->age));
+		$this->email = htmlspecialchars(strip_tags($this->email));
+		$this->tel_number = htmlspecialchars(strip_tags($this->tel_number));
+		
+		
+		$stmt->bind_param("ssiis", $this->firstname, $this->lastname, $this->age, $this->email, $this->tel_number);
+		
+		if($stmt->execute()){
+			return true;
+		}
+	 
+		return false;		 
+	}
+		
+	function update(){
+	 
+		$stmt = $this->conn->prepare("
+			UPDATE ".$this->itemsTable." 
+			SET name= ?, description = ?, price = ?, category_id = ?, created = ?
+			WHERE id = ?");
+	 
+		$this->id = htmlspecialchars(strip_tags($this->id));
+		$this->name = htmlspecialchars(strip_tags($this->name));
+		$this->description = htmlspecialchars(strip_tags($this->description));
+		$this->price = htmlspecialchars(strip_tags($this->price));
+		$this->category_id = htmlspecialchars(strip_tags($this->category_id));
+		$this->created = htmlspecialchars(strip_tags($this->created));
+	 
+		$stmt->bind_param("ssiisi", $this->name, $this->description, $this->price, $this->category_id, $this->created, $this->id);
+		
+		if($stmt->execute()){
+			return true;
+		}
+	 
+		return false;
+	}
+	
+	function delete(){
+		
+		$stmt = $this->conn->prepare("
+			DELETE FROM ".$this->itemsTable." 
+			WHERE id = ?");
+			
+		$this->id = htmlspecialchars(strip_tags($this->id));
+	 
+		$stmt->bind_param("i", $this->id);
+	 
+		if($stmt->execute()){
+			return true;
+		}
+	 
+		return false;		 
+	}
 }
 ?>
