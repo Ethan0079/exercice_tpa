94bde7447bec124bb9c1d0d7d860c0b70084c81a
Ethan <marchand.ethan@bluewin.ch>
2022-03-31 11:41:27 +0200

    Finish the CREATE Contact

--- webservice/rest-api/Class/Contact.php
+++ webservice/rest-api/Class/Contact.php
@@ -1,4 +1,3 @@
-
 <?php
 class Contact {   
   
@@ -29,25 +28,25 @@ class Contact {
 	}
 	
 	function create(){
-		
 		$stmt = $this->conn->prepare("
 			INSERT INTO ".$this->contactTable."(`firstname`, `lastname`, `age`, `email`, `tel_number`)
 			VALUES(?,?,?,?,?)");
-		
+			
 		$this->firstname = htmlspecialchars(strip_tags($this->firstname));
 		$this->lastname = htmlspecialchars(strip_tags($this->lastname));
 		$this->age = htmlspecialchars(strip_tags($this->age));
 		$this->email = htmlspecialchars(strip_tags($this->email));
 		$this->tel_number = htmlspecialchars(strip_tags($this->tel_number));
-		
-		
-		$stmt->bind_param("ssiis", $this->firstname, $this->lastname, $this->age, $this->email, $this->tel_number);
+
+		$stmt->bind_param("ssiss", $this->firstname, $this->lastname, $this->age, $this->email, $this->tel_number);
 		
 		if($stmt->execute()){
 			return true;
 		}
-	 
-		return false;		 
+		
+		return false;
+		$stmt.close();
+
 	}
 		
 	function update(){
--- webservice/rest-api/config/Database.php
+++ webservice/rest-api/config/Database.php
@@ -2,12 +2,12 @@
 class Database{
 	
 	private $host  = 'bd-ethan-exercice-tpa.mysql.database.azure.com';
-    private $user  = 'ethan@bd-ethan-exercice-tpa';
-    private $password   = "Chouffes.Biloutes";
-    private $database  = "bd-ethan-exercice-tpa"; 
+    private $user  = 'ethan';
+    private $password   = 'Chouffes.Biloutes';
+    private $database  = 'ethan_db_tpa_exercice'; 
     
     public function getConnection(){		
-		$conn = new mysqli($this->host, $this->user, $this->password);
+		$conn = new mysqli($this->host, $this->user, $this->password, $this->database);
 
 		if($conn->connect_error){
 			die("Error failed to connect to MySQL: " . $conn->connect_error);
