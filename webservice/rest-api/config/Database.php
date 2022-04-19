9b05084cd66b6ecf26451da9cddd66fbb471e57e
Ethan <marchand.ethan@bluewin.ch>
2022-04-19 08:50:17 +0200

    Add SSL connection

--- webservice/rest-api/config/Database.php
+++ webservice/rest-api/config/Database.php
@@ -3,7 +3,19 @@ class Database{
 	
     public function getConnection(){
 		require "config.php";
-		$conn = new mysqli($host, $user, $password, $database);
+		// $conn = new mysqli($host, $user, $password, $database);
+		// // mysqli_ssl_set($conn, NULL, NULL, NULL, "C:/BaltimoreCyberTrustRoot.crt.pem"), NULL);
+		// $conn->ssl_set(NULL, NULL, "C:/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
+
+		//Initializes MySQLi
+		$conn = mysqli_init();
+
+		mysqli_ssl_set($conn,NULL,NULL, "C:/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
+
+		// Establish the connection
+		mysqli_real_connect($conn, $host, $user, $password, $database, 3306, NULL, MYSQLI_CLIENT_SSL);
+
+
 
 		if($conn->connect_error){
 			die("Error failed to connect to MySQL: " . $conn->connect_error);
